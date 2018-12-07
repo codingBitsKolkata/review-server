cd9e86d8b4452ae68ac790b313bbc82c9bef0672
codingBitsKolkata <45310401+codingBitsKolkata@users.noreply.github.com>
2018-12-07 10:48:58 +0530

    first commit

---
+++ src/main/java/com/orastays/review/reviewserver/ReviewServerApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.review.reviewserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServerApplication.class, args);
+	}
+}
